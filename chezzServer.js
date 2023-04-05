1e2c357c32fe7391c820c3efc6261a127365b9c2
Joel Joelsson <joeljoels93@gmail.com>
2023-04-05 11:51:51 +0200

    Timer is now calculated at server side and distributed to clients

--- chezzServer.js
+++ chezzServer.js
@@ -3,6 +3,9 @@ import { Server } from "socket.io";
 const io = new Server(9000, {
   cors: { origin: ["http://localhost:3000"] },
 });
+let timerInterval;
+let whiteTime;
+let blackTime;
 
 io.on("connection", (socket) => {
   console.log("Someone connected with socket id: " + socket.id);
@@ -30,11 +33,29 @@ io.on("connection", (socket) => {
 
   socket.on("gameData_to_server", (room, userColor, selectedTimeControl) => {
     socket.to(room).emit("gameData_to_client", userColor, selectedTimeControl);
+    switch (selectedTimeControl) {
+      case "classical":
+        whiteTime = 1800;
+        blackTime = 1800;
+        break;
+      case "rapid":
+        whiteTime = 600;
+        blackTime = 600;
+        break;
+      case "blitz":
+        whiteTime = 180;
+        blackTime = 180;
+        break;
+      default:
+        whiteTime = 60;
+        blackTime = 60;
+    }
   });
 
   socket.on(
     "piece_moved",
     (room, fromSquare, toSquare, pieceType, pieceColor) => {
+      provideTimeToClients(pieceColor, room);
       socket
         .to(room)
         .emit("opponent_moved", fromSquare, toSquare, pieceType, pieceColor);
@@ -43,6 +64,7 @@ io.on("connection", (socket) => {
 
   socket.on("resign", (room, userColor) => {
     socket.to(room).emit("opponent_resigns", userColor);
+    clearInterval(timerInterval);
   });
 
   socket.on("offer_draw", (room) => {
@@ -51,5 +73,22 @@ io.on("connection", (socket) => {
 
   socket.on("draw_accepted", (room) => {
     socket.to(room).emit("receive_draw_accepted");
+    clearInterval(timerInterval);
   });
 });
+
+function provideTimeToClients(pieceColor, room) {
+  clearInterval(timerInterval);
+  timerInterval = setInterval(() => {
+    if (pieceColor === "white") {
+      blackTime = blackTime - 1;
+    } else {
+      whiteTime = whiteTime - 1;
+    }
+    io.sockets.to(room).emit("time_from_server", whiteTime, blackTime);
+    if (blackTime === 0 || whiteTime === 0) {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+}
+
